3cffa527e2af86bba642d95844136c243055cebe
Cody James Reed <cody_j_reed@proton.me>
2026-01-18 14:43:34 -0500

    refactor and style header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 
 export default function Header() {
-    return <div>
-        <Link href="/">Home</Link>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
+    return <div className="text-white w-full z-10 absolute">
+        <nav className="flex container relative flex-wrap items-center justify-between mx-auto p-8">
+            <Link href="/" className="font-bold text-3xl">Home</Link>
+            <div className="space-x-4 text-xl">
+                <Link href="/performance">Performance</Link>
+                <Link href="/reliability">Reliability</Link>
+                <Link href="/scale">Scale</Link>
+            </div>
+        </nav>
     </div>
 }
